f5a57b3c5b20969500c333a2058a15f6cbc23202
AVADHESH GUPTA <akgkgmittal2015@gmail.com>
2020-08-08 11:43:38 +0530

    Update multi-url.py

--- multi-url.py
+++ multi-url.py
@@ -1,7 +1,9 @@
-import webbrowser
+import webbrowser #importing browser library 
 
-file1=open('urls.txt', 'r')
-file2=file1.readlines()
+file1=open('url.txt', 'r') # Open text file as read argument
+file2=file1.readlines() #Reading all urls line by line
 
+#inserting url in loop
 for urls in file2:
-	webbrowser.open(url);
+	webbrowser.open(urls);
+print ("Code Run Successfuly")
